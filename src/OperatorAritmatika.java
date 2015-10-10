ff6f46f6db3a0d356c04db0a5a95c9ad2f3b4ca1
Rinianggita <riniad555@yahoo.com>
2015-10-10 08:41:45 +0700

    versi 2.3.2

---
+++ src/OperatorAritmatika.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Owner
+ */
+public class OperatorAritmatika {
+    public int nilaiHasil;
+    
+    int penjumlahan (int nilai1, int nilai2){
+        nilaiHasil = nilai1 + nilai2;
+        return nilaiHasil;
+    }
+    
+    int pengurangan (int nilai1, int nilai2){
+        nilaiHasil = nilai1 - nilai2;
+        return nilaiHasil;
+    }
+    
+    int perkalian (int nilai1, int nilai2){
+        nilaiHasil = nilai1 * nilai2;
+        return nilaiHasil;
+    }
+    
+    double pembagian (double nilai1, double nilai2){
+        double Hasil = nilai1 / nilai2;
+        return nilaiHasil;
+    }
+}
